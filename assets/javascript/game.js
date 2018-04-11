6e70e577cf5c36add3fe1d47f9991a5c6f445726
sastolarski <sastolarski@gmail.com>
2018-04-11 15:44:21 -0500

    random number generator

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+//first thing needed is to select a letter from the alphabet
+
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+//computer selects random letter from the string 
+
+console.log(alphabet[Math.floor(Math.random() * alphabet.length)]);
+
