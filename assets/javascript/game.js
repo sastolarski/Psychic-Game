830ad2e342e26d9283ea1792185f83a2e8604d06
sastolarski <sastolarski@gmail.com>
2018-04-12 16:57:27 -0500

    game is kind of working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,24 +4,54 @@ var alphabet = "abcdefghijklmnopqrstuvwxyz";
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
-// var currentGuesses = ;
+var guessedLetters = [];
+
+var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+
 
 //computer selects random letter from the string 
+
+//every time a user guesses, subtract one from guesses left
+
+//display the user guess in current guesses array? and keep them all up on screen
+
+//if guesses left reaches 0, add one point to losses
+
+//if user guess is equal to computer guess, add one to wins and game restarts
 
 
 
 document.onkeyup = function(event) {
 
     var userGuess = event.key;
-    
-    var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
 
     console.log(computerGuess);
 
-    if (userGuess === computerGuess) {
-        (wins++)
-        console.log("you win");
+
+    if ((userGuess !== computerGuess) && (guessesLeft !== 1)) {
+        (guessesLeft--);
+      
+    } else if (userGuess === computerGuess) {
+        (wins++);
+        (guessesLeft = 9);
+        computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+       
+    } else if (guessesLeft === 1) {
+        (losses++);
+        (guessesLeft = 9);
+        computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
     }
+
+
+
+    var html =
+    
+    "<p>wins: " + wins + "</p>" +
+    "<p>losses: " + losses + "</p>" +
+    "<p>guesses left: " + guessesLeft + "</p>"
+    "<p>guessed letters: " + guessedLetters.join() + "</p>" ;
+    
+    document.querySelector("#psychicGame").innerHTML = html;
 
 
 
